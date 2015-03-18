34a748e031723366d383f2096adc315498c70e7d
Eshlee Romero <esh@any.tv>
2015-03-18 14:30:48 +0800

    fix indent

--- src/Freedom/Service.php
+++ src/Freedom/Service.php
@@ -1,19 +1,20 @@
 <?php
 
 class Freedom_Service {
-	protected $adapter;
-	protected $request;
+	  protected $adapter;
+	  protected $request;
 
-	public function __construct($client) {
+    public function __construct($client)
+    {
+		    $this->config = include('configuration.php');
 
-		$this->config = include('configuration.php');
+		    $this->adapter = $client;
+		    $this->request = new Freedom_HttpRequest($this->config['backend_server']['url'] . ':' . $this->config['backend_server']['port']);
+		    $this->request->addHeader(array('X-ACCESS-TOKEN' => $this->adapter->getAccessToken()));
+	  }
 
-		$this->adapter = $client;
-		$this->request = new Freedom_HttpRequest($this->config['backend_server']['url'] . ':' . $this->config['backend_server']['port']);
-		$this->request->addHeader(array('X-ACCESS-TOKEN' => $this->adapter->getAccessToken()));
-	}
-/*
-	public function require($param) {
-		
-	}*/
+    public function requires($param)
+    {
+
+    }
 }
