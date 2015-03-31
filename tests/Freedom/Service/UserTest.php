56a81d37c6f751781c8c852a9db29653a1e879e7
perseusl <perseus@any.tv>
2015-03-31 10:46:51 +0800

    added update prospect

--- tests/Freedom/Service/UserTest.php
+++ tests/Freedom/Service/UserTest.php
@@ -258,7 +258,7 @@ class Freedom_Service_UserTest extends BaseTest
 
 	public function testUpdateProspect()
 	{
-		/*if(!$this->userService) {
+		if(!$this->userService) {
 			$this->testConnection();
 		}
 
@@ -269,7 +269,7 @@ class Freedom_Service_UserTest extends BaseTest
 			$payload = $req['payload'];
 			$updateProspect = $this->userService->updateProspect($prospect_id, $payload);
 
-		} skipped, got stucked :( */
+		}
 	}
 
 	public function testDeleteProspects()
