0e39f6d2720c78da16281d218565321bbf6dfde7
perseusl <perseus@any.tv>
2015-03-31 10:20:01 +0800

    revert

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,22 +1,6 @@
 <?php
 
-
-$autoloadPath = __DIR__;
-if(strrpos($autoloadPath, '/')) {
-	$autoloadPath = explode('/', $autoloadPath);
-	array_pop($autoloadPath);
-	$autoloadPath = implode('/', $autoloadPath);
-} else {
-	$autoloadPath = explode('\\', $autoloadPath);
-	array_pop($autoloadPath);
-	$autoloadPath = implode('\\', $autoloadPath);
-}
-
-echo __DIR__;
-echo "============================";
-var_dump($autoloadPath);
-
-require_once $autoloadPath . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/BaseTest.php';
 //set a valid access token here . . .
 //$valid
