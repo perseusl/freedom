5a0367aede9d8376bb97dd74f4f8916c84116576
perseusl <perseus@any.tv>
2015-03-31 10:35:20 +0800

    remove comment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,4 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/BaseTest.php';
-//set a valid access token here . . .
-//$valid
-/*global $myToken;
-echo var_dump($_GET['accessToken']);
-$_GET['accessToken'] = $myToken;*/
+
